4e0352ae2b7a3c92b5482a84e953159959cf85e0
LiorKorssia22 <korssia2@gmail.com>
2022-11-05 17:40:17 +0200

    e-commerce

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const productListLeft = document.querySelector(".product-list-left");
+const productListLeftInside = document.querySelector(".productIn");
+
 const productListRight = document.querySelector(".product-list-right");
-const productCheck = document.querySelector(".check");
 
 //do const to same parameter
 //משתנים ברורים
 
-function htmlTemp(id,name,url) 
- {
-    `
-    <div class = "product">
-    <div class = "product-content">
-    <h3 >id: ${id}</h3>
-    <span >site name : ${name}</span>
-    <br/>
-    <a href="${url}" target="_blank" >site url: ${name}</a>
-    </div>
-    </div>
-    `
-}; 
+// function htmlTemp(id, name, url) {
+//     `
+//     <div class = "product">
+//     <div class = "product-content">
+//     <h3 >id: ${id}</h3>
+//     <span >site name : ${name}</span>
+//     <br/>
+//     <a href="${url}" target="_blank" >site url: ${name}</a>
+//     </div>
+//     </div>
+//     `
+// };
 
 
 // load product items content form JSON file
@@ -27,89 +27,69 @@ const createHtml = async () => {
         .then((data) => {
             let htmlLeft = "";
             let htmlRight = "";
-            let htmlRight1 = "";
-            let ans = "";
+            let productLeftInside = "";
 
             //check colum 1
-            // console.log(data[0]);
-            // console.log(data[0]['subData']);
-
-
-     
-
-            ans = JSON.stringify(data[1]);
-
-         
-
-         
-
-            if (data[0].hasOwnProperty('subData')) {
-                // console.log(data[0].subData);
-
-            }
-
-
-            //show num1
-            htmlLeft += `<div class = "product-item">
-            <div class = "product-content">
-            <h3 >id: ${data[0].id}</h3>
-            <span >site name : ${data[0].name}</span>
+            console.log(data[0]);
+            //show id 1
+            htmlLeft += `<div class = "firstId">
+            <div>
+            <span>id: ${data[0].id}</span> 
             <br/>
-            <a href="${data[0].url}" target="_blank" >site url: ${data[0].name}</a>
+            <span>site name : ${data[0].name}</span>
+            <br/>
+            <a href="${data[0].url}" target="_blank">site url: ${data[0].name}</a>
             </div>
             </div>`;
 
-            // if (data[0].hasOwnProperty('subData')) {
-            //     ans = "work1";
-            // }
-            // else { ans = "not work1"; }
-            productCheck.innerHTML = ans;
 
-            //show first "subData"
+
+            //show 2,3,7
             data[0].subData.forEach(items => {
                 const { id, name, url } = items;
                 htmlLeft += `
-                    <div class = "product">
-                    <div class = "product-content">
-                    <h3 >id: ${id}</h3>
+                <div class = "productInInside">
+                <div class = "product">
+                    <span >id: ${id}</span>
+                    <br/>
                     <span >site name : ${name}</span>
                     <br/>
-                    <a href="${url}" target="_blank" >site url: ${name}</a>
+                    <a href="${url}" target="_blank">site url: ${name}</a>
                     </div>
                     </div>
                     `;
+                //show 4,5,6
                 if (typeof items.subData !== "undefined") {
-
                     items.subData.forEach(item => {
                         const { id, name, url } = item;
-
-                        htmlLeft +=  `
-                        <div class = "product">
-                        <div class = "product-content">
-                        <h3 >id: ${id}</h3>
-                        <span >site name : ${name}</span>
+                        htmlLeft += `
+                       
+                        <div class = "productIn">
+                        <div >
+                        <span>id: ${id}</span>
                         <br/>
-                        <a href="${url}" target="_blank" >site url: ${name}</a>
+                        <span>site name : ${name}</span>
+                        <br/>
+                        <a href="${url}" target="_blank">site url: ${name}</a>
                         </div>
                         </div>
+                        
                         `;
                     })
                 }
             });
-
-
-            productListLeft.innerHTML = htmlLeft + "-";
-
+            // productListLeftInside.innerHTML = productLeftInside;
+            productListLeft.innerHTML = htmlLeft;
 
 
             //check colum 2
-            // console.log(data[1]);
-            //show num8
+            //show 8
             htmlRight += `
-                     <div class = "product-item">
+                     <div class = "firstId">
                                           
                      <div class = "product-content">
-                      <h3 >id: ${data[1].id}</h3>
+                      <span >id: ${data[1].id}</span>
+                      <br/>
                       <span >site name : ${data[1].name}</span>
                       <br/>
                    <a href="${data[1].url}" target="_blank" >site url: ${data[1].name}</a>
@@ -118,61 +98,56 @@ const createHtml = async () => {
                       `;
 
 
-            //show first "subData"
+            //show 9,10,17,21
             data[1].subData.forEach(items => {
                 const { id, name, url } = items;
 
                 htmlRight += `
                     <div class = "product">
-                                            
                     <div class = "product-content">
-                    <h3 >id: ${id}</h3>
-                      <span >site name : ${name}</span>
+                    <span >id: ${id}</span>
                     <br/>
-                   <a href="${url}" target="_blank" >site url: ${name}</a>
-                   </div>
+                    <span >site name : ${name}</span>
+                    <br/>
+                    <a href="${url}" target="_blank" >site url: ${name}</a>
+                    </div>
                     </div>
                   `;
-
+                //show 11,15,16,,18,19,20
                 if (typeof items.subData !== "undefined") {
                     items.subData.forEach(item => {
                         const { id, name, url } = item;
-                        if(typeof item.subData !== "undefined")
-                        {   
-                            item.subData.forEach(prod => {
-                                const { id, name, url } = prod;
-                            htmlRight += `
-                            <div class = "product">
-                            <div class = "product-content">
-                            <h3 >id: ${id}</h3>
-                            <span >site name : ${name}</span>
-                            <br/>
-                            <a href="${url}" target="_blank" >site url: ${name}</a>
-                            </div>
-                            </div>
-                            `;   
-                             
-                            })
-                        }
-
-                        console.log(items.subData);
                         htmlRight += `
-                        <div class = "product">
+                        <div class = "productIn">
                         <div class = "product-content">
-                        <h3 >id: ${id}</h3>
+                        <span >id: ${id}</span>
+                        <br/>
                         <span >site name : ${name}</span>
                         <br/>
                         <a href="${url}" target="_blank" >site url: ${name}</a>
                         </div>
                         </div>
                         `;
-
-
+                        //show 12,13,14
+                        if (typeof item.subData !== "undefined") {
+                            item.subData.forEach(prod => {
+                                const { id, name, url } = prod;
+                                htmlRight += `
+                            <div class = "productInIn">
+                            <div>
+                            <span >id: ${id}</span>
+                            <br/>
+                            <span >site name : ${name}</span>
+                            <br/>
+                            <a href="${url}" target="_blank" >site url: ${name}</a>
+                            </div>
+                            </div>
+                            `;
+                            })
+                        }
                     });
                 }
             });
-
-
             productListRight.innerHTML = htmlRight;
         })
         .catch((error) => {
